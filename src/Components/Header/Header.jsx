780b460a3bee7066e316f3b3eac33f375074b069
dev_bro <tohirbabs@gmail.com>
2022-04-05 23:33:06 +0100

    update header

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -2,7 +2,20 @@ import React from 'react'
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <div className='header'>
+        <div className='header__logo'></div>
+
+        <div className='header__links'>
+            <div className='header__nav'>
+                <ul>
+                    <li><a href='#' className='header__link'>Join Moveables</a></li>
+                    <li><a href='#' className='header__link'>Contact Us</a></li>
+                </ul>
+            </div>
+
+            <button className='header__cta'>Request Access</button>
+        </div> 
+    </div>
   )
 }
 
