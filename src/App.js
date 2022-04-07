db01cf5c5b7dfa650067c85fa1ae2d7ba3ea605d
dev_bro <tohirbabs@gmail.com>
2022-04-07 09:20:09 +0100

    updates

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import Hero from './Components/Hero/Hero';
+import Benefits from './Components/Benefits/Benefits';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <Hero />
+      <Benefits />
     </div>
   );
 }
---
+++ src/Components/Benefits/Benefits.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+
+import benefits from './benefits.png'
+
+const Benefits = () => {
+  return (
+    <section className="benefits" >
+        <div className='benefits__img'>
+            <img ></img>
+        </div>
+        <div className='benefits__text'>
+            <h2>what will you get?</h2>
+            <h3>More Clients</h3>
+            <p>We aim to be able to connect customer and service provider seamlessly.</p>
+            <h3>Integration</h3>
+            <p>We aim to be able to connect customer and service provider seamlessly.</p>
+            <h3>Efficient Customer Service</h3>
+            <p>We aim to be able to connect customer and service provider seamlessly.</p>
+            <h3>Efficient Customer Service</h3>
+            <p>We aim to be able to connect customer and service provider seamlessly.</p>
+        </div>
+    </section>
+  )
+}
+
+export default Benefits
