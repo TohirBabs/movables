c5f4c7dc481a60fd339e276cbbcd778ad249ae5b
dev_bro <tohirbabs@gmail.com>
2022-04-07 08:03:58 +0100

    update

--- src/Components/Hero/Hero.js
+++ src/Components/Hero/Hero.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import Header from './Header/Header'
+import Welcome from './Welcome/Welcome'
 import './Hero.css'
 
 const Hero = () => {
   return (
     <div className='Hero'>
         <Header />
+        <Welcome />
     </div>
   )
 }
---
+++ src/Components/Hero/Welcome/Welcome.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+import './Welcome.css'
+
+const Welcome = () => {
+  return (
+    <div className='welcome flex'>
+        <h2>Become a service Provider on movables</h2>
+        <button>Join Movables</button>
+    </div>
+  )
+}
+
+export default Welcome
