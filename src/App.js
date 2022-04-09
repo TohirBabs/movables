a1b708e7a6eb9ff5eabc27b152985c74df301054
dev_bro <tohirbabs@gmail.com>
2022-04-09 15:54:23 +0100

    update

--- src/App.js
+++ src/App.js
@@ -12,8 +12,8 @@ function App() {
   return (
     <div className="App">
       <Hero />
-      {/* <Benefits />
-      <Work />
+      <Benefits />
+      {/* <Work />
       <Join />
       <Waitlist />
       <Together />
