84edb4a711176a8b151d8a86fdf103da77e03672
dev_bro <tohirbabs@gmail.com>
2022-04-13 10:24:20 +0100

    updates

--- src/App.js
+++ src/App.js
@@ -15,8 +15,8 @@ function App() {
       <Benefits />
       <Work />
       <Join />
-      {/* <Waitlist />
-      <Together />
+      <Waitlist />
+      {/* <Together />
       <Footer /> */}
     </div>
   );
